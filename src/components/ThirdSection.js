546dc79132ac871f83368500d0b5db585d8a7dc7
eunandocosta <fmscosta99@gmail.com>
2023-05-22 17:33:33 -0300

    fernandoCostaCurriculo

--- src/components/ThirdSection.js
+++ src/components/ThirdSection.js
@@ -2,15 +2,14 @@ import React from 'react'
 import {DiReact, DiJavascript1, DiNodejsSmall, DiPython, DiHtml5, DiCss3, DiCode, DiMysql} from "react-icons/di"
 import { FaLanguage } from "react-icons/fa"
 import { SiMicrosoftoffice } from "react-icons/si"
+import { TbBrandReactNative } from "react-icons/tb"
+import { BsGithub } from "react-icons/bs"
 
 const ThirdSection = React.forwardRef((props, ref) => {
     function createCard(name, level, Icon){
         function cases(level){
             if(parseInt(level)<30){
                 const xp = 'Básico'
-                return xp
-            }else if(parseInt(level)<50){
-                const xp = 'Experiente'
                 return xp
             }else if(parseInt(level)<80){
                 const xp = 'Intermediário'
@@ -42,11 +41,13 @@ const ThirdSection = React.forwardRef((props, ref) => {
         <div className='flex gap'>
            { createCard('JavaScript', '60', DiJavascript1)}
            { createCard('React', '80', DiReact)}
+           { createCard('React Native', '45', TbBrandReactNative)}
            { createCard('NodeJS', '50', DiNodejsSmall)}
            { createCard('Python', '65', DiPython)}
            { createCard('HTML', '90', DiHtml5)}
            { createCard('CSS', '90', DiCss3)}
            { createCard('C', '25', DiCode)}
+           { createCard('GitHub', '15', BsGithub)}
            { createCard('MySql', '45', DiMysql)}
         </div>
         <div className='title'>Conhecimentos Gerais</div>
